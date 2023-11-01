ae5081d099e1732d378c064b940bcad85acb24a9
Daniel Ávila <avilaqdaniel@gmail.com>
2023-11-01 18:48:34 -0500

    Descarga de internet con wget

---
+++ 10_descargar.sh
@@ -0,0 +1,6 @@
+# ! /bin/bash
+# Programa para ejemplificar el uso de la descarga de internet usando el comando wget
+# Autor: Daniel Ávila - @avilaqdaniel
+
+echo "Descargar información de internet"
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.82/bin/apache-tomcat-9.0.82.zip
